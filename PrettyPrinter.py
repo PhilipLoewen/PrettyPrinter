782b30b23cdd5fd0d42fc96ed8b149880f4f6e92
Philip D Loewen <loew@math.ubc.ca>
2024-06-28 22:51:43 -0700

    Improve handling for 3D arrays.

--- PrettyPrinter.py
+++ PrettyPrinter.py
@@ -1,28 +1,52 @@
+# PrettyPrinter
+
+# (c) Philip D Loewen, 2024-06-28
+
 import numpy as np
-import math
 
 def ppm(M, name="",**kwargs):
+    """ppm - Pretty-print a numpy ndarray, provided ndim is not too large.
 
+       (One could say more here.)
+    """
     # Main keyword argument is "sigfigs".
-
-    if not isinstance(M,np.ndarray):
-        print(f"ppm works on objects with class numpy.ndarray, ")
-        print(f"but the given object has class {M.__class__.__name__}. No output.")
-        return(None)
-
-    if len(M.shape)!=2:
-        # If the numpy array is just 1D, try to make it a 1-column matrix
-        rowcount = math.prod(M.shape)
-        MMM = M.reshape(rowcount,1)
-        print(f"Warning: Input ndarray has shape {M.shape}. Printing it as a column.")
-    else:
-        MMM = M
-    (rowcount,colcount) = MMM.shape
 
     if len(name)==0:
         padname=" "
+        parname=" "
     else:
         padname=" "+name+" "
+        parname=", namely "+name+", "
+
+    if not isinstance(M,np.ndarray):
+        print(f"Error: ppm works on objects with class numpy.ndarray, ")
+        print(f"but the given object{parname}has class {M.__class__.__name__}.")
+        print("No output.")
+        return(None)
+
+    if M.ndim == 1:
+        MMM = M.reshape(M.shape[0],1)
+        print(f"Presenting 1D array{padname}in shape {M.shape[0]}x1",end="")        
+    elif M.ndim == 2:
+        MMM = M
+        print(f"Matrix{padname}has shape {M.shape[0]}x{M.shape[1]}",end="")
+    elif M.ndim == 3 and min(M.shape) == 1:
+        if M.shape[1]==1:
+            MMM = M.reshape(M.shape[0],M.shape[2])
+            print(f"Presenting 3D array{padname}as a lineup of {M.shape[2]} columns, ",end="")
+            print(f"each with {M.shape[0]} rows",end="")
+        elif M.shape[0]==1:
+            MMM = M.T.reshape(M.shape[2],M.shape[1])
+            print(f"Presenting 3D array{padname}as a pile of {M.shape[2]} rows, ",end="")
+            print(f"each with {M.shape[1]} cols",end="")
+        else:
+            MMM = M.reshape(M.shape[0],M.shape[1])
+            print(f"Presenting 3D array{padname}as a single {M.shape[0]}x{M.shape[1]} matrix",end="")
+    else:
+        MMM = M.reshape(M.size,1)
+        print(f"Warning: Input array{padname}is {M.ndim}-dimensional, with shape {M.shape}. ",end="")
+        print(f"Printing it as one column with {M.size} rows",end="")
+    (rowcount,colcount) = MMM.shape
 
     s = int(kwargs.get('sigfigs',4))   # Default number of sig figs is hard-coded here
     s = max([1,s])                     # Silently repair bogus request
@@ -41,21 +65,21 @@ def ppm(M, name="",**kwargs):
 
     if r>0 and d>0:
         # Nominal case worked. Formulate format code and print
-        print(f"Matrix{padname}has size {rowcount}x{colcount}:")
+        print(f":")
         fmt = "{"+f":{s+2:d}.{d:d}f"+"}"
         for r in range(rowcount):
             print(" "+" ".join([fmt.format(q) for q in MMM[r,:]]))
     elif r==0 and relax:
         # Stretch case. All sig figs follow "0.".
         # Just allow an extra character column for cosmetic reasons.
-        print(f"Matrix{padname}has size {rowcount}x{colcount}:")
+        print(f":")
         fmt = "{"+f":{s+3:d}.{d:d}f"+"}"
         for r in range(rowcount):
             print(" "+" ".join([fmt.format(q) for q in MMM[r,:]]))
     else:
         # Scale the whole matrix so at least one entry shows all sig figs
         p = r - 1
-        print(f"Matrix{padname}has size {rowcount}x{colcount};")
+        print(f";")
         print(f" ... multiply each value below by 1E{p}:")
         fmt = "{"+f":{s+2:d}.{s-1:d}f"+"}"
         for r in range(rowcount):
--- test-ppm.py
+++ test-ppm.py
@@ -63,13 +63,51 @@ print(" ")
 C = np.pi
 ppm(C,"C")
 
+print(" ")
+
 A10000 = A0*10000
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=6)
+
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=5)
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=4)
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=3)
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=2)
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=1)
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=0)
+print(" ")
+
 ppm(A10000,"A10000 = A0 * 10000",sigfigs=48)
+print(" ")
+
+yyy = np.random.rand(1,7,3)*1E7
+ppm(yyy,"yyy")
+ppm(yyy[:,:,2],"yyy[:,:,2]")
+
+print(" ")
+
+zzz = np.random.rand(3,1,4)*1E-5
+ppm(zzz,"zzz")
+ppm(zzz[:,:,2],"zzz[:,:,2]")
+
+print(" ")
+
+# It turns out that a 0-dimensional array is possible,
+# and it has ndim=0, size=1, and reshaping it just works.
+# The following demonstrates this.
+
+scalar = np.array(666.)
+ppm(scalar,"scalar")
+
 
